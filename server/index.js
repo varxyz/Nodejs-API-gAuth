4566b8e9e8db4edf35d3328e16ba2c3cc8b30aa3
varxyz <mrsergiumoraru@gmail.com>
2018-04-05 21:30:05 +0300

    added gAuth config added

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/auth');
-const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
 
