533c6d8b5d704576dd2e115d67d044462e9ab12e
varxyz <mrsergiumoraru@gmail.com>
2018-04-07 10:43:10 +0300

    modified MONGOD env for prod

--- server/config/prod.js
+++ server/config/prod.js
@@ -5,6 +5,6 @@ module.exports = {
   //without heroku you need to specify the full db address here. Make a new user on mlab
   //and use the credentials in the link
   //eg: mongodb://<dbuser>:<dbpassword>@ds135399.mlab.com:35399/YOUR_DB_NAME
-  mongoURI: process.env.MONGO_URI,
+  mongoURI: process.env.MONGODB_URI,
   cookieKey: process.env.COOKIE_KEY
 };
